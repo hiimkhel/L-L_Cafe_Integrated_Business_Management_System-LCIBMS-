e0cc60c8f35cac38b2484039536fa96e4f326749
Arvin Bearneza <bearnezaarvin19@gmail.com>
2026-03-21 23:47:12 +0800

    Feat- Added tertiary color variable

--- frontend/lib/config/theme/app_colors.dart
+++ frontend/lib/config/theme/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   static const Color primary = Color(0xFFA98258);  // coffee brown
   static const Color secondary = Color(0xFF758C6D);  // green
   static const Color background = Color(0xFFEFE2C9);  // cream
+  static const Color tertiary = Color(0xFFa98258);
 
   // extras
   static const Color textDark = Color(0xFF2B2B2B);
