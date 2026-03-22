336f143504f0b98576b61bb1ba4b903174f2e615
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-03-22 17:58:14 +0800

    Applied sidebar widget to dashboard screen

--- frontend/lib/features/dashboard/presentation/admin/dashboard_screen.dart
+++ frontend/lib/features/dashboard/presentation/admin/dashboard_screen.dart
@@ -1,39 +1,45 @@
 import 'package:flutter/material.dart';
+import '../../../../core/widgets/admin_sidebar.dart';
+import "../../../../config/theme/app_colors.dart";
 import 'menu_management.dart';
 import 'order_screen.dart';
 
-class AdminDashboardScreen extends StatelessWidget {
-  const AdminDashboardScreen({super.key});
+class AdminDashboardScreen extends StatefulWidget {
+  final int activeIndex;
+  const AdminDashboardScreen({super.key, this.activeIndex = 0});
+  @override
+  State<AdminDashboardScreen> createState() => _AdminDashboardScreenState();
+}
+
+class _AdminDashboardScreenState extends State<AdminDashboardScreen>{
+  late int activeIndex;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const Center(child: Text('Admin Dashboard')),
+  void initState(){
+    super.initState();
+    activeIndex = widget.activeIndex;
+  }
 
-          ListTile(
-            leading: const Icon(Icons.menu_book),
-            title: const Text("Menu Management"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const MenuManagementScreen()),
-              );
-            },
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Row(
+        children: [
+          Sidebar(activeIndex: activeIndex),
+          Expanded(
+            child: Center(
+              child: Text(
+                'This is the Dashboard Screen',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.tertiary,
+                ),
+              ),
+            ),
           ),
-          ListTile(
-            leading: const Icon(Icons.shopping_bag),
-            title: const Text("Order Management"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const OrderScreen()),
-              );
-            },
-          ),
-        ],
-      ),
+        ]
+      )
     );
   }
 }
