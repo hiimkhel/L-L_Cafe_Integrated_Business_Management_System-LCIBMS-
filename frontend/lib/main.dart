ade60de9aae5bad93f771edc318092725a2db822
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-03-22 18:58:41 +0800

    Added routing system for admin screens with temporary screens for Menu and orders

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -7,6 +7,11 @@ import 'features/dashboard/presentation/cashier/dashboard_screen.dart';
 import 'features/home/presentation/rider/home_screen.dart';
 import 'features/auth/presentation/screens/login_screen.dart';
 import 'features/dashboard/presentation/admin/menu_management.dart';
+import 'features/cms/presentation/cms_screen.dart';
+import 'features/customers/presentation/admin/customers_screen.dart';
+import 'features/reports/presentation/admin/reports_screen.dart';
+import 'features/reviews/presentation/admin/reviews_screen.dart';
+
 
 void main() {
   runApp(const LCIBMSApp());
@@ -34,13 +39,13 @@ final List<User> fakeUsers = [
 
 // Routes for Admin Screens
 final Map<String, Widget Function(BuildContext) > adminRoutes = {
-  '/dashboard': (_) => const MenuManagementScreen(activeIndex: 0),
-  '/orders': (_) => const MenuManagementScreen(activeIndex: 1),
-  '/menu_management': (_) => const MenuManagementScreen(activeIndex: 2),
-  '/reports': (_) => const MenuManagementScreen(activeIndex: 3),
-  '/customers': (_) => const MenuManagementScreen(activeIndex: 4),
-  '/reviews': (_) => const MenuManagementScreen(activeIndex: 5),
-  '/cms': (_) => const MenuManagementScreen(activeIndex: 6),
+  '/dashboard': (_) => const AdminDashboardScreen(activeIndex: 0),
+  '/orders': (_) => const CMSScreen(activeIndex: 1),
+  '/menu_management': (_) => const CMSScreen(activeIndex: 2),
+  '/reports': (_) => const ReportsScreen(activeIndex: 3),
+  '/customers': (_) => const CustomersScreen(activeIndex: 4),
+  '/reviews': (_) => const ReviewsScreen(activeIndex: 5),
+  '/cms': (_) => const CMSScreen(activeIndex: 6),
    // Add other routes here
 };
  
@@ -100,7 +105,7 @@ class _LCIBMSAppState extends State<LCIBMSApp> {
           debugShowCheckedModeBanner: false,
           initialRoute: '/dashboard',
           routes: adminRoutes, // <-- our named routes for admin
-          home: const MenuManagementScreen(activeIndex: 0),
+          home: const AdminDashboardScreen(activeIndex: 0),
         );
     }
   }
