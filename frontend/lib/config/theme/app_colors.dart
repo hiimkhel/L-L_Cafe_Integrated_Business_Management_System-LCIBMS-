17b3975313d3f08a981c6d5dc79e6bdec1b3f063
Keshyue <davidandre.solomon@wvsu.edu.ph>
2026-03-23 14:00:39 +0800

    add color for card and textstyle small

--- frontend/lib/config/theme/app_colors.dart
+++ frontend/lib/config/theme/app_colors.dart
@@ -5,8 +5,10 @@ class AppColors {
   static const Color secondary = Color(0xFF758C6D);  // green
   static const Color background = Color(0xFFEFE2C9);  // cream
   static const Color tertiary = Color(0xFFa98258);
+  static const Color card = Color.fromARGB(255, 255, 255, 255);
 
   // extras
   static const Color textDark = Color(0xFF2B2B2B);
   static const Color textLight = Color(0xFFFFFFFF);
+  static const Color border = Color(0xFFA98258);
 }
