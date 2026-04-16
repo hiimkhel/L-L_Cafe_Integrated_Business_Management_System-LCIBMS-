ba51f5d09977edd00e2bcc12f0a9291472d43071
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-04-16 20:55:00 +0800

    Refractor flutter models to reflect our final users schema

--- frontend/lib/core/models/user.dart
+++ frontend/lib/core/models/user.dart
@@ -1,11 +1,22 @@
-// User Roles
-enum UserRole { customer, admin, pos, rider }
+enum UserRole { admin, cashier, rider, customer }
 
-// Hardcoded User model
 class User {
   final String email;
   final String password;
   final UserRole role;
 
   User(this.email, this.password, this.role);
+}
+
+UserRole stringToRole(String role) {
+  switch (role) {
+    case 'admin':
+      return UserRole.admin;
+    case 'cashier':
+      return UserRole.cashier;
+    case 'rider':
+      return UserRole.rider;
+    default:
+      return UserRole.customer;
+  }
 }
