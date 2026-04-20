d9a82f7aade4256e873ed2b74708bde304bdffa3
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-04-20 13:51:59 +0800

    Refractor routes to utilize controllers folder

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,33 +7,10 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/dbConnection.js');
+const { login, register } = require("../controllers/authControllers");
 
 
-router.post('/login', async (req, res) => {
-  const { firebase_uid } = req.body;
-
-  try {
-    const [rows] = await db.execute(
-      'SELECT * FROM user WHERE firebase_uid = ?',
-      [firebase_uid]
-    );
-
-    if (rows.length === 0) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    const user = rows[0];
-
-    res.json({
-      email: user.email,
-      full_name: user.full_name,
-      role: user.role
-    });
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+router.post('/login', login);
+router.post('/register', register);
 
 module.exports = router;
