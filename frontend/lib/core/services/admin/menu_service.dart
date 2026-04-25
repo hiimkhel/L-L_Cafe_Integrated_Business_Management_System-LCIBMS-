34d5a62a543b2ea9517822df231397925f5e1a98
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-04-25 18:25:28 +0800

    Add admin menu services dart to handle api calls for backend

---
+++ frontend/lib/core/services/admin/menu_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class MenuService {
+  static const String baseUrl = "http://localhost:3006/api/admin";
+
+  // ---------------- CATEGORIES ----------------
+  static Future<List<dynamic>> fetchCategories() async {
+    final res = await http.get(Uri.parse('$baseUrl/menu/category'));
+
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw Exception("Failed to load categories");
+    }
+  }
+
+  // ---------------- MENU ITEMS ----------------
+  static Future<List<dynamic>> fetchMenuItems(int categoryId) async {
+    final res = await http.get(
+      Uri.parse('$baseUrl/menu-items?category_id=$categoryId'),
+    );
+
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw Exception("Failed to load menu items");
+    }
+  }
+}
