7c088fc637d60ff015c1a7270ab88737608a9b59
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-04-29 22:31:33 +0800

    Add backend route and logic for fetching customers full profile

--- backend/controllers/customerControllers.js
+++ backend/controllers/customerControllers.js
@@ -1,5 +1,43 @@
 const db = require("../config/dbConnection.js");
 
+const getUserProfile = async (req, res) => {
+
+    try{
+        const userId = req.params.id;
+
+        // 1. Get user info
+        const [userRows] = await db.query(
+        `SELECT id, full_name, email, phone, profile_picture, created_at
+        FROM users WHERE id = ?`,
+        [userId]
+        );
+
+        if (userRows.length === 0) {
+        return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+        // 2. Get addresses
+        const [addressRows] = await db.query(
+        `SELECT id, label, full_address 
+        FROM user_addresses 
+        WHERE user_id = ?`,
+        [userId]
+        );
+
+        // 3. Combine response
+        res.json({
+        success: true,
+        user: userRows[0],
+        addresses: addressRows
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, message: err.message });
+    }
+    
+}
+
 const getUserAddresses = async (req, res) => {
     try{
         const userId = req.params.id;
@@ -17,4 +55,4 @@ const getUserAddresses = async (req, res) => {
 
 
 
-module.exports = { getUserAddresses };
+module.exports = { getUserAddresses, getUserProfile};
--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getUserAddresses } = require("../controllers/customerControllers.js");
+const { getUserAddresses, getUserProfile } = require("../controllers/customerControllers.js");
 
+router.get("/:id", getUserProfile);
 router.get("/:id/addresses", getUserAddresses);
 
 module.exports = router;
