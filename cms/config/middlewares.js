af67a0bbd41d958ea6ae292e2472c079adec65f7
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-05-01 05:49:02 +0800

    Add max files for request in cms

--- cms/config/middlewares.js
+++ cms/config/middlewares.js
@@ -5,8 +5,19 @@ module.exports = [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: "256mb",    // Max size for form data
+      jsonLimit: "256mb",    // Max size for JSON data
+      textLimit: "256mb",
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // 250mb individual file limit (in bytes)
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  
 ];
