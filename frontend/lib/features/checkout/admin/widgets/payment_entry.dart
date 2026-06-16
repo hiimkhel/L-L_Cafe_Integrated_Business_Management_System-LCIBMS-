01fcca00dfd71e1d3c4a08f711ba8d1ee5e31354
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-06-16 17:52:50 +0800

    Add receipt model import to payment entry file

--- frontend/lib/features/checkout/admin/widgets/payment_entry.dart
+++ frontend/lib/features/checkout/admin/widgets/payment_entry.dart
@@ -3,6 +3,7 @@ import 'package:frontend/config/theme/app_colors.dart';
 import 'package:frontend/config/theme/app_text_styles.dart';
 import 'package:frontend/core/widgets/receipt.dart';
 import 'package:frontend/features/orders/presentation/pos/screens/order_queue_screen.dart';
+import 'package:frontend/core/models/receipt_model.dart';
 
 class PaymentEntry extends StatefulWidget {
   final double total;
